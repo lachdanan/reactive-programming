5133fcaba5b11b70c866cdbfa3df3751a944c850
Eduardo <lachdanan_3@hotmail.com>
2024-12-04 18:23:54 -0600

    Added StepVerifier to unit tests

--- src/test/kotlin/com/example/reactiveexamples/domain/PersonRepositoryImplTest.kt
+++ src/test/kotlin/com/example/reactiveexamples/domain/PersonRepositoryImplTest.kt
@@ -1,6 +1,7 @@
 package com.example.reactiveexamples.domain
 
 import org.junit.jupiter.api.Test
+import reactor.test.StepVerifier
 
 class PersonRepositoryImplTest {
 
@@ -10,18 +11,25 @@ class PersonRepositoryImplTest {
     fun getById() {
         val p1Mono = personRepositoryImpl.getById(1)
         val p1 = p1Mono.block()
+        StepVerifier.create(p1Mono).expectNextCount(1).verifyComplete()
         println(p1.toString())
     }
 
     @Test
     fun getByIdSubscribe() {
         val p1Mono = personRepositoryImpl.getById(1)
+
+        StepVerifier.create(p1Mono).expectNextCount(1).verifyComplete()
+
         p1Mono.subscribe { println(it.toString())}
     }
 
     @Test
     fun getByIdMapSubscribe() {
         val p1Mono = personRepositoryImpl.getById(1)
+
+        StepVerifier.create(p1Mono).expectNextCount(1).verifyComplete()
+
         p1Mono.map {
             it.firstName
         }.subscribe { println("firstName: $it") }
@@ -31,18 +39,21 @@ class PersonRepositoryImplTest {
     fun getByIdWithFluxBlock() {
         val p1Flux = personRepositoryImpl.findAll()
         val person = p1Flux.blockFirst()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         println(person.toString())
     }
 
     @Test
     fun getAllFluxSubscribe() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         p1Flux.subscribe { println(it.toString()) }
     }
 
     @Test
     fun getAllFluxToListMono() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         val monoList = p1Flux.collectList()
         monoList.subscribe {
             println(it.toString())
@@ -52,13 +63,16 @@ class PersonRepositoryImplTest {
     @Test
     fun getAllFluxPlayGround() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         p1Flux.any { it.firstName == "Antonia" }.subscribe { println(it)}
     }
 
     @Test
     fun getAllFluxWithFilter() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         val p1Mono = p1Flux.filter { it.id == 3 }.next()
+        StepVerifier.create(p1Mono).expectNextCount(1).verifyComplete()
         p1Mono.subscribe {
             println(it.toString())
         }
@@ -67,7 +81,9 @@ class PersonRepositoryImplTest {
     @Test
     fun getAllFluxWithFilterAndNotFound() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         val p1Mono = p1Flux.filter { it.id == 4 }.next()
+        StepVerifier.create(p1Mono).expectNextCount(0).verifyComplete()
         p1Mono.subscribe {
             println(it.toString())
         }
@@ -76,7 +92,9 @@ class PersonRepositoryImplTest {
     @Test
     fun getAllFluxWithFilterAndNotFoundAndThrowsException() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         val p1Mono = p1Flux.filter { it.id == 4 }.single()
+        StepVerifier.create(p1Mono).expectNextCount(0).verifyError()
         p1Mono.subscribe {
             println(it.toString())
         }
@@ -85,7 +103,9 @@ class PersonRepositoryImplTest {
     @Test
     fun getAllFluxWithFilterAndNotFoundAndHandlingException() {
         val p1Flux = personRepositoryImpl.findAll()
+        StepVerifier.create(p1Flux).expectNextCount(3).verifyComplete()
         val p1Mono = p1Flux.filter { it.id == 4 }.single()
+        StepVerifier.create(p1Mono).expectNextCount(0).verifyError()
         p1Mono.doOnError {
             println("Do on Error")
         }.onErrorReturn(Person(4,"Mr.", "White")).subscribe {
@@ -96,6 +116,7 @@ class PersonRepositoryImplTest {
     @Test
     fun findById() {
         val personMono = personRepositoryImpl.getById(2)
+        StepVerifier.create(personMono).expectNextCount(1).verifyComplete()
         personMono.subscribe { println(it.toString()) }
 
     }
@@ -103,6 +124,7 @@ class PersonRepositoryImplTest {
     @Test
     fun findByIdWithNoIdFoundAndReturnEmptyMono() {
         val personMono = personRepositoryImpl.getById(4)
+        StepVerifier.create(personMono).expectNextCount(0).verifyComplete()
         personMono.subscribe { println(it.toString()) }
     }
 }
